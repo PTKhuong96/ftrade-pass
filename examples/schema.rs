0f543f15926b13128ea900d3e240a92212374d3c
PTKhuong96 <phankhuong96@gmail.com>
2022-08-10 03:38:56 +0000

    Update schema.rs

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,18 +3,15 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use ftrade_pass::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use ftrade_pass::state::State;
+use ftrade_employee_ct::msg::{ ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema");
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
-
+    
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CountResponse), &out_dir);
 }
